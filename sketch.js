0d3c06dba9bb730edb91b4dde2f4dd225e549941
Chenyating24 <167044579+Chenyating24@users.noreply.github.com>
2024-04-29 10:28:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
+var captureGraphics
+var capture_width=640
+var capture_height=480
+var span=10
+
 function setup() { 
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO) //啟動攝影機
-  capture.size(320,240);//設定顯示畫面大小
+  capture.size(capture_width,capture_height);//設定顯示畫面大小
+  captureGraphics=captureGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
 }
 
 function draw() {
   background(220);
+  noStroke()
   push()
-    translate(width/2-160, height/2-120) //把原點移到畫面中間
-    image(capture,0,0) //在(mouseX, mouseY)顯示capture
+    translate(capture_width/2-capture_width/2, capture_height/2-capture_height/2) //把原點移到畫面中間
+    captureGraphics.image(capture,0,0) //在(mouseX, mouseY)顯示capture
+    for(var x=0;x<captureGraphics.width;x=x+span){
+      for(var y=0;y<captureGraphics.height;y=y+span){
+        var pixel=captureGraphics.get(x,y)
+        fill(pixel)
+        rect(x,y,span)
+      }
+    }
   pop()
 }
